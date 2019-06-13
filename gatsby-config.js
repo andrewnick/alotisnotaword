88a2e5fa1a24fcc9c2e0e60010dbd7f40afe3098
Andrew Nicholas <andrew.d.nicholas@gmail.com>
2019-06-14 08:09:28 +1200

    basic text

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Alot is not a word`,
+    description: `Alot is not a word`,
+    author: `@andrew`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,13 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="container">
+      <div className="text">
+        Alot is <strong>not</strong> a word
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
   </Layout>
 )
 
