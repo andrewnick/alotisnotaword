772096146eecc85ef572c38d17a89726abdb1bd5
Andrew Nicholas <andrew@vyg.co.nz>
2019-06-14 14:45:56 +1200

    add site icon

--- src/components/image.js
+++ src/components/image.js
@@ -14,19 +14,20 @@ import Img from "gatsby-image"
  */
 
 const Image = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
-  />
+  <div></div>
+  // <StaticQuery
+  //   query={graphql`
+  //     query {
+  //       placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+  //         childImageSharp {
+  //           fluid(maxWidth: 300) {
+  //             ...GatsbyImageSharpFluid
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `}
+  //   render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+  // />
 )
 export default Image
